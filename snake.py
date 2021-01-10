a13e1c594d7a4b8dd7703a63c7ecc03f5d842f5a
Vinícius Oliveira <vinicius.oliveira1911@gmail.com>
2021-01-10 17:20:25 -0400

    Update snake.py

--- snake.py
+++ snake.py
@@ -8,8 +8,7 @@ def on_grid_random():
     return (x//10 *10, y //10 *10)
 
 def collision(c1, c2):
-    return(c1[0] == c2[0]) and (c2[1] == c2[1])
-
+    return(c1[0] == c2[0]) and (c1[1] == c2[1])
 UP = 0
 RIGHT = 1
 DOWN = 2
@@ -35,7 +34,7 @@ font = pygame.font.Font('freesansbold.ttf', 18)
 score = 0
 
 game_over = False
-while True:
+while not game_over:
     clock.tick(15)
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -54,6 +53,7 @@ while True:
     if collision(snake[0], apple_pos):
         apple_pos = on_grid_random()
         snake.append((0, 0))
+        score = score + 1
     
     if snake[0][0] == 600 or snake[0][1] == 600 or snake[0][0] < 0 or snake[0][1] < 0:
         game_over = True
@@ -90,10 +90,24 @@ while True:
     
     score_font = font.render('Score: %s' % (score), True, (255, 255, 255))
     score_rect = score_font.get_rect()
-    score_rect.topleft = (600 - 120, 10)
+    score_rect.topleft = (520 - 120, 10)
     screen.blit(score_font, score_rect)
 
     for pos in snake:
         screen.blit(snake_skin, pos)
 
     pygame.display.update()
+
+while True:
+    game_over_font = pygame.font.Font('freesansbold.ttf', 75)
+    game_over_screen = game_over_font.render('Game Over', True, (255, 255, 255))
+    game_over_rect = game_over_screen.get_rect()
+    game_over_rect.midtop = (500 / 2, 10)
+    screen.blit(game_over_screen, game_over_rect)
+    pygame.display.update()
+    pygame.time.wait(500)
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
